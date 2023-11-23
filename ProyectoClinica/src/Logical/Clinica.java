091baa0c41988a729e74e4bb04902eba5eb738e7
shigo <shigo@192.168.8.176>
2023-11-23 12:57:53 -0400

    arregle algo en clinica

--- ProyectoClinica/src/Logical/Clinica.java
+++ ProyectoClinica/src/Logical/Clinica.java
@@ -7,13 +7,22 @@ public class Clinica {
 	private ArrayList<Medico> misMedico;
 	private ArrayList<Paciente> misPaciente;
 	private ArrayList<Consultas> misConsultas;
+	private static Clinica clinica = null;
 	
-	public Clinica(ArrayList<Medico> misMedico, ArrayList<Paciente> misPaciente, ArrayList<Consultas> misConsultas) {
+	public Clinica() {
 		super();
-		this.misMedico = misMedico;
-		this.misPaciente = misPaciente;
-		this.misConsultas = misConsultas;
+		this.misMedico = new ArrayList<>();
+		this.misPaciente = new ArrayList<>();
+		this.misConsultas = new ArrayList<>();
 	}
+	
+	public static Clinica getInstance(){
+		if(clinica == null){
+			clinica = new Clinica();
+		}		
+		return clinica;
+	} 
+	
 
 	public ArrayList<Medico> getMisMedico() {
 		return misMedico;
@@ -39,7 +48,4 @@ public class Clinica {
 		this.misConsultas = misConsultas;
 	}
 	
-	
-	
-
 }

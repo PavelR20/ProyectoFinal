362b92ad0ab96b30515c761dcc64d45da5f6f5d5
Jean Perez <Jean Perez@USUARIO-N8BR339>
2023-11-23 13:33:54 -0800

    Complete la clase cita

--- ProyectoClinica/src/Logical/Cita.java
+++ ProyectoClinica/src/Logical/Cita.java
@@ -1,5 +1,52 @@
 package Logical;
 
+import java.sql.Date;
+
 public class Cita {
+	private String idCita;
+	private Paciente paciente;
+	private Medico doc;
+	private Date fecha;
+	
+	public Cita(String idCita, Paciente paciente, Medico doc, Date fecha) {
+		super();
+		this.idCita = idCita;
+		this.paciente = paciente;
+		this.doc = doc;
+		this.fecha = fecha;
+	}
+
+	public String getIdCita() {
+		return idCita;
+	}
+
+	public void setIdCita(String idCita) {
+		this.idCita = idCita;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Medico getDoc() {
+		return doc;
+	}
+
+	public void setDoc(Medico doc) {
+		this.doc = doc;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
 
 }

bf621d05569c54bf824542b24c72c6ae45be30ee
shigo <shigo@192.168.8.176>
2023-11-23 12:49:54 -0400

    agregue mas cosas a clinica

--- ProyectoClinica/src/Logical/Clinica.java
+++ ProyectoClinica/src/Logical/Clinica.java
@@ -1,5 +1,45 @@
 package Logical;
 
+import java.util.ArrayList;
+
 public class Clinica {
+	
+	private ArrayList<Medico> misMedico;
+	private ArrayList<Paciente> misPaciente;
+	private ArrayList<Consultas> misConsultas;
+	
+	public Clinica(ArrayList<Medico> misMedico, ArrayList<Paciente> misPaciente, ArrayList<Consultas> misConsultas) {
+		super();
+		this.misMedico = misMedico;
+		this.misPaciente = misPaciente;
+		this.misConsultas = misConsultas;
+	}
+
+	public ArrayList<Medico> getMisMedico() {
+		return misMedico;
+	}
+
+	public void setMisMedico(ArrayList<Medico> misMedico) {
+		this.misMedico = misMedico;
+	}
+
+	public ArrayList<Paciente> getMisPaciente() {
+		return misPaciente;
+	}
+
+	public void setMisPaciente(ArrayList<Paciente> misPaciente) {
+		this.misPaciente = misPaciente;
+	}
+
+	public ArrayList<Consultas> getMisConsultas() {
+		return misConsultas;
+	}
+
+	public void setMisConsultas(ArrayList<Consultas> misConsultas) {
+		this.misConsultas = misConsultas;
+	}
+	
+	
+	
 
 }

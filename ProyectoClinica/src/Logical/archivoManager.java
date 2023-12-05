066ac5eb9110a2e205f9737168ef0ce120a020e8
shigo <shigo@192.168.8.176>
2023-12-05 11:58:32 -0400

    fix

--- ProyectoClinica/src/Logical/archivoManager.java
+++ ProyectoClinica/src/Logical/archivoManager.java
@@ -93,28 +93,28 @@ public class archivoManager {
 		}
 	}
 
-	public static void leerPaciente() {
+	public static ArrayList<Paciente> leerPacientes() {
+		ArrayList<Paciente> pacientes = new ArrayList<>();
+		
 		 try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH2))) {
 		        String line;
 		        while ((line = reader.readLine()) != null) {
 		            String[] parts = line.split(",");
 		            if (parts.length == 10) {
-		                Paciente paciente = new Paciente(parts[0], parts[1], parts[2], parseDate(parts[3]), parts[4],
-		                        parts[5], new Vivienda(parts[6], parts[7], parts[8], new ArrayList<Persona>()),
-		                        parts[9], new HistoriaMedica(new ArrayList<>()));
+		            	
+		            	 Vivienda vivienda = new Vivienda(parts[6], parts[7], parts[8], new ArrayList<Persona>());
+		                 Paciente paciente = new Paciente(parts[0], parts[1], parts[2], parseDate(parts[3]), parts[4],
+		                         parts[5], vivienda, parts[9], new HistoriaMedica(new ArrayList<>()));
 		                
-		                String[] consultas = parts[9].split(";");
-		                for (String consultaStr : consultas) {
-		                    String[] consultaParts = consultaStr.split(":");
-		                    //paciente.getHistorial().getHistorialConsultas().add(consultas);
-		                }
-
-		                System.out.println(paciente);
+		                pacientes.add(paciente);
+		                
 		            }
 		        }
 		    } catch (IOException e) {
 		        e.printStackTrace();
 		    }
+		 
+		 return pacientes;
 	}
 
 	// Vacuna 
